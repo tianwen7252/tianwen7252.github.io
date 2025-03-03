60183e923bc31338a11bd5a6cd3aa8a3907e4608
tianwen7252 <tianwen7252@gmail.com>
2025-03-03 17:56:07 +0800

    fix: soups were not correct

--- src/constants/defaults/config.ts
+++ src/constants/defaults/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   MAIN_DISH_TYPE: 'main-dish',
-  NO_NEED_SOUP_KEYWORDS: ['沙拉'],
+  NO_NEED_SOUP_KEYWORDS: ['加蛋', '加菜', '沙拉'],
   // DRINK_KEYWORDS: ['飲料'],
   AUTO_SHOW_ORDER_LIST: true,
   SHOW_SOUPS_NUMBER: true,
